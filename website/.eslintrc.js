428fcc671dd370a832646cf0b6e12e19db88c18b
Manas Talukdar <manas.talukdar@gmail.com>
2019-09-07 15:51:39 -0700

    Updated.

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -9,30 +9,32 @@ module.exports = {
   },
   extends: [
     'plugin:prettier/recommended',
-    "eslint:recommended",
+    'eslint:recommended',
+    'google',
     'standard'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
-    "no-underscore-dangle": 0,
-    "import/imports-first": ["error", "absolute-first"],
-    "import/newline-after-import": "error",
-    "prettier/prettier": ["error", {
-      "endOfLine": "auto"
-    }],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-underscore-dangle': 0,
+    'import/imports-first': ['error', 'absolute-first'],
+    'import/newline-after-import': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
-  "globals": {
-    "window": true,
-    "document": true,
-    "localStorage": true,
-    "FormData": true,
-    "FileReader": true,
-    "Blob": true,
-    "navigator": true
+  globals: {
+    window: true,
+    document: true,
+    localStorage: true,
+    FormData: true,
+    FileReader: true,
+    Blob: true,
+    navigator: true
   }
 }
